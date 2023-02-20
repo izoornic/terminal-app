c6b6d5f110518029ff21fe3035c2ef07fb42ce13
Ivan Zornic <izornic@gmail.com>
2023-02-20 18:13:19 +0100

    V 0.3.0.2

--- resources/views/blacklist.php
+++ resources/views/blacklist.php
@@ -9,11 +9,12 @@ dd(Helpers::datumKalendarNow()); */
 $url = Storage::url('blacklist.txt');
 $contents = Storage::disk('public')->get('blacklist.txt');
 //dd ($contents);
+echo asset('storage/blacklist.txt');
 
-header('Content-Disposition: attachment; filename="blacklist.txt"');
+/* header('Content-Disposition: attachment; filename="blacklist.txt"');
 header('Content-Type: text/plain'); // Don't use application/force-download - it's not a real MIME type, and the Content-Disposition header is sufficient
 header('Content-Length: ' . strlen($contents));
-header('Connection: close');
+header('Connection: close'); */
 
 echo $contents;
 
